4b9462774dce1a5be087e0e0683408591eb28630
AkuroP <thomas.kang2002@gmail.com>
2022-09-06 16:43:41 +0200

    Grenade + renvoi / explosion

--- GameAndWars/Assets/Scripts/GameManager.cs
+++ GameAndWars/Assets/Scripts/GameManager.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> positions;
-    public int currentPosition = 2;
+    public List<GameObject> grenadeTopLeftPos;
+    public List<GameObject> grenadeTopRightPos;
+    public List<GameObject> grenadeBottomLeftPos;
+    public List<GameObject> grenadeBottomRightPos;
 
+    private List<GameObject> grenadePos;
+    public int currentPosition = 2;
+    private int grenadePosition = 0;
+    private int reversePos = -1;
 
     //Controles
 
     private float cooldownControl = 0.7f;
+    private float cooldownGrenade = 0.7f;
+
+    private bool isLaunching = false;
+    private bool isReverse = false;
 
     private void Start()
     {
@@ -18,10 +29,56 @@ public class GameManager : MonoBehaviour
         {
             if (positions.IndexOf(position) != currentPosition) position.GetComponent<Animator>().Play("Deactivating");
         }
+
+        for(int i = 0; i < grenadeTopLeftPos.Count; i++)
+        {
+            grenadeTopLeftPos[i].GetComponent<Animator>().Play("Deactivating");
+            grenadeTopRightPos[i].GetComponent<Animator>().Play("Deactivating");
+            grenadeBottomLeftPos[i].GetComponent<Animator>().Play("Deactivating");
+            grenadeBottomRightPos[i].GetComponent<Animator>().Play("Deactivating");
+        }
     }
 
     private void Update()
     { 
+
+        //Grenade
+        if(Input.GetKeyDown(KeyCode.Space) && isLaunching == false)
+        {
+            int random = Random.Range(0, 4);
+            switch(random)
+            {
+                case 0 :
+                    grenadePos = grenadeTopLeftPos;
+                break;
+                case 1 :
+                    grenadePos = grenadeTopRightPos;
+                break;
+                case 2 :
+                    grenadePos = grenadeBottomLeftPos;
+                break;
+                case 3 :
+                    grenadePos = grenadeBottomRightPos;
+                break;
+            }
+            reversePos = random;
+            isLaunching = true;
+            cooldownGrenade = 0.7f;
+            grenadePos[grenadePosition].GetComponent<Animator>().Play("Activating");
+        }
+
+        if(isLaunching == true)
+        {
+            if(cooldownGrenade > 0)
+            {
+                cooldownGrenade -= Time.deltaTime;
+            }
+            else
+            {
+                if(isReverse == false) CheckGrenade(false);
+                else CheckGrenade(true);
+            }
+        }
 
         if (cooldownControl > 0)
         {
@@ -86,6 +143,57 @@ public class GameManager : MonoBehaviour
 
             positions[currentPosition].GetComponent<Animator>().Play("Activating");
 
+        }
+    }
+
+    private void CheckGrenade(bool reverse)
+    {
+        if(reverse == false)
+        {
+            if(grenadePosition < grenadeTopLeftPos.Count - 1)
+            {
+                grenadePos[grenadePosition].GetComponent<Animator>().Play("Deactivating");
+                grenadePosition += 1;
+                cooldownGrenade = 0.7f;
+                grenadePos[grenadePosition].GetComponent<Animator>().Play("Activating");
+            }
+            else
+            {
+                if(reversePos == currentPosition)
+                {
+                    grenadePosition = grenadeTopLeftPos.Count - 1;
+                    isReverse = true;
+                    Debug.Log("REVERSE");
+                    CheckGrenade(true);
+                }
+                else
+                {
+                    grenadePos[grenadePosition].GetComponent<Animator>().Play("Deactivating");
+                    Debug.Log("BOUM");
+                    isLaunching = false;
+                    grenadePosition = 0;
+                    cooldownGrenade = 0.7f;
+                }
+                reversePos = -1;
+            }
+        }
+        else
+        {
+            if(grenadePosition > 0)
+            {
+                grenadePos[grenadePosition].GetComponent<Animator>().Play("Deactivating");
+                cooldownGrenade = 0.5f;
+                grenadePosition -= 1;
+                grenadePos[grenadePosition].GetComponent<Animator>().Play("Activating");
+            }
+            else
+            {
+                grenadePosition = 0;
+                grenadePos[grenadePosition].GetComponent<Animator>().Play("Deactivating");
+                cooldownGrenade = 0.7f;
+                isLaunching = false;
+                isReverse = false;
+            }
         }
     }
 
